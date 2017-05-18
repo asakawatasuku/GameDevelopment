ebbb7540905c1e4822e0d66397d7143a1e84fb79
asakawatasuku <akuma.fairy.tail@gmail.com>
2017-05-18 16:27:46 +0900

    JoyPadクラスの更新

--- GameDevelopment/JoyPad.cpp
+++ GameDevelopment/JoyPad.cpp
@@ -14,20 +14,39 @@ struct SearchJoyPadSet
 	}
 };
 
+//BOOL CALLBACK EnumObjectsCallback(LPCDIDEVICEOBJECTINSTANCE pInstance,LPVOID pvRef)
+//{
+//	// 次を検索
+//	return DIENUM_CONTINUE;
+//}
+
 // 検出したジョイパッド後と呼ばれるコールバック
 BOOL CALLBACK EnumJoyPadCallBack(LPCDIDEVICEINSTANCE lpddi,void* pvRef)
 {
+	// 結果
 	HRESULT ret;
+
 	SearchJoyPadSet* param = reinterpret_cast<SearchJoyPadSet*>(pvRef);
 	LPDIRECTINPUTDEVICE8 dev;
 	// コントローラー毎にデバイスを生成
 	ret = param->dinput->CreateDevice(lpddi->guidInstance, &dev,NULL);
-	// 失敗
+	// 生成に失敗
 	if (ret != DI_OK)
 	{
 		goto NEXT;
 	}
 
+	// データ形式を設定(拡張機能付き)
+	dev->SetDataFormat(&c_dfDIJoystick2);
+	// 生成に失敗
+	if (ret != DI_OK)
+	{
+		goto NEXT;
+	}
+
+	// 配列に追加
+	param->JoyPadDev.push_back(dev);
+	
 NEXT:
 	// 次を検索
 	return DIENUM_CONTINUE;
@@ -39,6 +58,12 @@ JoyPad::JoyPad()
 
 JoyPad::~JoyPad()
 {
+	// 配列にあるデバイスを開放
+	std::vector<JoyPadSet>::iterator it;
+	for (it = m_joyPadSet.begin(); it != m_joyPadSet.end(); it++)
+	{
+		SafeRelease(it->inputDevice);
+	}
 }
 
 // 初期化関数
@@ -48,8 +73,7 @@ bool JoyPad::initialize(HWND window)
 
 	// DirectInputの作成
 	ret = DirectInput8Create(GetModuleHandle(NULL), DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&m_DInput, NULL);
-
-	// 失敗
+	// 生成に失敗
 	if (ret != DI_OK)
 	{
 		return false;
@@ -57,17 +81,83 @@ bool JoyPad::initialize(HWND window)
 
 	// 受け渡し用変数
 	SearchJoyPadSet param;
-
 	param.dinput = m_DInput;
 
 	// 利用可能なデバイスの列挙
 	ret = m_DInput->EnumDevices(DI8DEVCLASS_GAMECTRL, EnumJoyPadCallBack, &param, DIEDFL_ATTACHEDONLY);
-
-	// 失敗
+	// 生成に失敗
 	if (ret != DI_OK)
 	{
 		return false;
 	}
 
+	// デバイス配列を設定
+	std::vector<LPDIRECTINPUTDEVICE8>::iterator it;
+	for (it = param.JoyPadDev.begin(); it != param.JoyPadDev.end(); it++)
+	{
+		LPDIRECTINPUTDEVICE8 dev = *it;
+
+		// アプリがアクティブ時はデバイスを排他アクセスに設定
+		ret = dev->SetCooperativeLevel(window, DISCL_EXCLUSIVE | DISCL_FOREGROUND);
+		// 失敗
+		if (ret != DI_OK)
+		{
+			continue;
+		}
+
+		// アクセス権を要求
+		ret = dev->Acquire();
+		// 失敗
+		if (ret != DI_OK)
+		{
+			continue;
+		}
+
+		// デバイス1個分の情報
+		JoyPadSet initPad;
+		initPad.inputDevice = dev;
+		// 配列に追加
+		m_joyPadSet.push_back(initPad);
+	}
+
 	return true;
+}
+
+void JoyPad::update()
+{
+	// 全てのデバイスについて処理する
+	std::vector<JoyPadSet>::iterator it;
+	for (it = m_joyPadSet.begin(); it != m_joyPadSet.end(); it++)
+	{
+		HRESULT ret;
+
+		// 情報を更新
+		it->inputDevice->Poll();
+		if (ret != DI_OK)
+		{
+			// アクセス権を要求
+			ret = it->inputDevice->Acquire();
+			while (ret == DIERR_INPUTLOST)
+			{
+				// アクセス権を要求
+				ret = it->inputDevice->Acquire();
+			}
+		}
+
+		// 前の情報
+		it->joypadOld = it->joypad;
+
+		// 入力情報を取得
+		ret = it->inputDevice->GetDeviceState(sizeof(DIJOYSTATE2), &it->joypad);
+		// 失敗
+		if (ret != DI_OK)
+		{
+			continue;
+		}
+
+		if ((BYTE)(it->joypad.rgbButtons[0] & 0x80 == 0))
+		{
+			// ボタンが押されている
+		}
+	}
 }
--- GameDevelopment/JoyPad.h
+++ GameDevelopment/JoyPad.h
@@ -8,14 +8,45 @@
 #include "dinput.h"
 #include <vector>
 
+// テンプレート
+template <typename T>
+void SafeRelease(T*& p)
+{
+	if (p)
+	{
+		p->Release();
+		p = NULL;
+	}
+}
+
 class JoyPad
 {
 private:
+
+	// ジョイパッド1個分の情報
+	struct JoyPadSet {
+		JoyPadSet() {
+			inputDevice = NULL;
+		}
+		// デバイス
+		LPDIRECTINPUTDEVICE8 inputDevice;
+		// 入力情報
+		DIJOYSTATE2 joypad;
+		// 前回の入力情報
+		DIJOYSTATE2 joypadOld;
+	};
+
+	// DirectInputインスタンス
 	LPDIRECTINPUT8 m_DInput;
+
+	// ジョイパッド配列
+	std::vector<JoyPadSet> m_joyPadSet;
 
 public:
 	JoyPad();
 	~JoyPad();
+	// 初期化関数
 	bool initialize(HWND window);
-	
+	// 更新関数
+	void update();
 };
